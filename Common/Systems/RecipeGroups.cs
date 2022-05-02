593aad512890757bb9ae02f49778f4f8671f17c6
RQXY <emfr3n@gmail.com>
2022-05-02 13:03:25 -0400

    Create RecipeGroups.cs

---
+++ Common/Systems/RecipeGroups.cs
@@ -0,0 +1,21 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Localization;
+
+namespace AltLibrary.Common.Systems
+{
+    internal class RecipeGroups : ModSystem
+    {
+        public static RecipeGroup group;
+        public override void AddRecipeGroups()
+        {
+            group = new RecipeGroup(() => Language.GetTextValue("LegacyMisc.37") + "Evil Bar", new int[]
+            {
+                ItemID.DemoniteBar,
+                ItemID.CrimtaneBar
+            });
+            RecipeGroup.RegisterGroup("AltLibrary:EvilBars", group);
+        }
+    }
+}
