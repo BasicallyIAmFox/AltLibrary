72fe758cfae0cf8c3944bd972079e22003a9d64d
RQXY <emfr3n@gmail.com>
2022-05-03 14:27:46 -0400

    Create debug hallow alt

---
+++ Content/EpicSwagBiome.cs
@@ -0,0 +1,21 @@
+﻿using AltLibrary.Common.AltBiomes;
+using Terraria.ID;
+
+namespace AltLibrary.Content
+{
+    internal class EpicSwagBiome : AltBiome
+    {
+        public override void SetStaticDefaults()
+        {
+            BiomeType = BiomeType.Hallow;
+            BiomeGrass = TileID.GoldBrick;
+            BiomeStone = TileID.Gold;
+            BiomeSand = TileID.YellowStucco;
+            BiomeIce = TileID.TeamBlockYellow;
+            BiomeSandstone = TileID.PlatinumBrick;
+            MechDropItemType = ItemID.GoldBar;
+        }
+
+        public override string IconLarge => "AltLibrary/Assets/WorldIcons/EpicSwag";
+    }
+}
