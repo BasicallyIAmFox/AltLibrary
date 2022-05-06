2da18fde3f3d390bb18c42f46f854c2c90ff1245
RQXY <emfr3n@gmail.com>
2022-05-05 23:00:07 -0400

    Add Fox dev vanity

---
+++ Content/DevArmor/Fox/FoxMask.cs
@@ -0,0 +1,29 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+using Terraria.Localization;
+
+namespace AltLibrary.Content.DevArmor.Fox
+{
+    [AutoloadEquip(EquipType.Head)]
+    internal class FoxMask : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Fox's Face Mask");
+            Tooltip.SetDefault("Great for impersonating mod devs!");
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+            ArmorIDs.Head.Sets.DrawFullHair[Item.headSlot] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 24;
+            Item.height = 14;
+            Item.value = Item.sellPrice(gold: 5);
+            Item.rare = ItemRarityID.Cyan;
+            Item.vanity = true;
+        }
+    }
+}
---
+++ Content/DevArmor/Fox/FoxShirt.cs
@@ -0,0 +1,27 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+
+namespace AltLibrary.Content.DevArmor.Fox
+{
+    [AutoloadEquip(EquipType.Body)]
+    internal class FoxShirt : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Fox's Sweater");
+            Tooltip.SetDefault("Great for impersonating mod devs!");
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 24;
+            Item.height = 14;
+            Item.value = Item.sellPrice(gold: 5);
+            Item.rare = ItemRarityID.Cyan;
+            Item.vanity = true;
+        }
+    }
+}
