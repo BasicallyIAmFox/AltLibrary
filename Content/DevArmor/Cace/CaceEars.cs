d6ba7ca463bb4970d0e02ae63ab8d035a9628580
RQXY <emfr3n@gmail.com>
2022-05-06 17:47:00 -0400

    Add Cace dev vanity item

---
+++ Content/DevArmor/Cace/CaceEars.cs
@@ -0,0 +1,28 @@
+﻿using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AltLibrary.Content.DevArmor.Cace
+{
+    [AutoloadEquip(EquipType.Head)]
+    internal class CaceEars : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Cace's Cat Ears");
+            Tooltip.SetDefault("Great for impersonating mod devs!");
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+            ArmorIDs.Head.Sets.DrawFullHair[Item.headSlot] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 24;
+            Item.height = 26;
+            Item.value = Item.sellPrice(gold: 5);
+            Item.rare = ItemRarityID.Cyan;
+            Item.vanity = true;
+        }
+    }
+}
