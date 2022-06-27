4de3aea63e45a039af7883513389b17842290b0a
Yreorel <68769795+Ouroel@users.noreply.github.com>
2022-06-27 21:11:56 +0800

    Create AltLibraryGlobalItem.cs

---
+++ AltLibraryGlobalItem.cs
@@ -0,0 +1,58 @@
+using AltLibrary.Common.AltBiomes;
+using AltLibrary.Common.Systems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AltLibrary
+{
+    internal class AltLibraryGlobalItem : GlobalItem
+    {
+        public static Dictionary<int, bool> HallowedOreList;
+        public override void OnSpawn(Item item, IEntitySource source)
+        {
+            if (WorldBiomeManager.WorldHallow == "")
+                return;
+            EntitySource_TileBreak tile = source as EntitySource_TileBreak;
+            if (tile != null && HallowedOreList.ContainsKey(Main.tile[tile.TileCoords].TileType))
+            {
+                AltBiome biome = AltLibrary.Biomes.Find(x => x.FullName == WorldBiomeManager.WorldHallow);
+                item.SetDefaults(biome.MechDropItemType.Value);
+            }
+        }
+
+        class AltLibraryOre_Loader : ILoadable
+        {
+            public void Load(Mod mod)
+            {
+                On.Terraria.WorldGen.OreRunner += OreRunner_ReplaceHallowedOre;
+                HallowedOreList = new Dictionary<int, bool>();
+            }
+
+
+            public void Unload()
+            {
+                On.Terraria.WorldGen.OreRunner -= OreRunner_ReplaceHallowedOre;
+                HallowedOreList = null;
+            }
+            private static void OreRunner_ReplaceHallowedOre(On.Terraria.WorldGen.orig_OreRunner orig, int i, int j, double strength, int steps, ushort type)
+            {
+                if (WorldBiomeManager.WorldHallow == "")
+                    return;
+                if (HallowedOreList.ContainsKey(type))
+                {
+                    AltBiome biome = AltLibrary.Biomes.Find(x => x.FullName == WorldBiomeManager.WorldHallow);
+                    if (biome.BiomeOre != null)
+                        type = (ushort)biome.BiomeOre.Value; 
+                }
+                orig(i, j, strength, steps, type);
+            }
+        }
+    }
+}
